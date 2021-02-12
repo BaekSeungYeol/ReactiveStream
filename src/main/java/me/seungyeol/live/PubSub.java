322ebf6813b5b4a85cc2ed6eb0266b574eed06e1
백승열 <beck33333@naver.com>
2021-02-12 21:43:40 +0900

    Operator before refactoring

--- src/main/java/me/seungyeol/live/PubSub.java
+++ src/main/java/me/seungyeol/live/PubSub.java
@@ -5,24 +5,100 @@ import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
 
-import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
- * Reactive Streams -
+ * Reactive Streams - Operators
+ * Publisher -> [Data1] -> Op1 -> [Data2] -> Op2 -> [Data3] -> Subscriber
+ * map ( d1 -> f - > d2 )
+ * Pub -> [Data1] ]-> mapPub -> [Data2] - >LogPub
+ *                  <- subscribe(logSub)
+ *                  -> OnSubscribe(s)
+ *                  -> onNext
+ *                  -> onNext
+ *                  -> onComplete
+ *
  */
 public class PubSub {
     public static void main(String[] args) {
-        Publisher<Integer> pub = new Publisher<Integer>() {
-            Iterable<Integer> iter = Stream.iterate(1, a -> a +1).limit(10).collect(Collectors.toList());
+        Publisher<Integer> pub = iterPub(Stream.iterate(1, a -> a +1).limit(10).collect(Collectors.toList()));
+        Publisher<Integer> mapPub = mapPub(pub,  s -> s * 10);
+        Publisher<Integer> mapPub2 = mapPub(mapPub,  s -> -s);
+        mapPub2.subscribe(LogSub());
+    }
 
+    private static Publisher<Integer> mapPub(Publisher<Integer> pub, Function<Integer, Integer> f) {
+        return new Publisher<Integer>() {
+            @Override
+            public void subscribe(Subscriber<? super Integer> sub) {
+                pub.subscribe(new Subscriber<Integer>() {
+                    @Override
+                    public void onSubscribe(Subscription s) {
+                        sub.onSubscribe(s);
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                     sub.onNext(f.apply(integer));
+                    }
+
+                    @Override
+                    public void onError(Throwable t) {
+                        sub.onError(t);
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        sub.onComplete();
+                    }
+                });
+            }
+        };
+    }
+
+    private static Subscriber<Integer> LogSub() {
+        return new Subscriber<Integer>() {
+            @Override
+            public void onSubscribe(Subscription s) {
+                System.out.println("onSubscribe");
+                s.request(Long.MAX_VALUE);
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                System.out.println("onNext:" + integer);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                System.out.println("onError");
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("onComplete");
+                //log.info("onComplete");
+            }
+        };
+    }
+
+    private static Publisher<Integer> iterPub(List<Integer> iter) {
+        return new Publisher<Integer>() {
             @Override
             public void subscribe(Subscriber<? super Integer> sub) {
                 sub.onSubscribe(new Subscription() {
                     @Override
                     public void request(long n) {
+                        try {
+                            iter.forEach(s -> sub.onNext(s));
+                            sub.onComplete();
 
+                        }catch (Throwable t) {
+                            sub.onError(t);
+                        }
                     }
 
                     @Override
@@ -32,29 +108,5 @@ public class PubSub {
                 });
             }
         };
-
-        Subscriber<Integer> sub = new Subscriber<Integer>() {
-            @Override
-            public void onSubscribe(Subscription s) {
-
-            }
-
-            @Override
-            public void onNext(Integer integer) {
-
-            }
-
-            @Override
-            public void onError(Throwable t) {
-
-            }
-
-            @Override
-            public void onComplete() {
-
-            }
-        };
-
-        pub.subscribe(sub);
     }
 }
